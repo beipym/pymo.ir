20a0175972b49de556f7d976e443c174d418df26
beipym <payamb.wrk@gmail.com>
2025-05-09 00:22:29 +0330

    remove trash scenes as we only need one scene

--- src/bg-interactive-2d/scenes/game.ts
+++ src/bg-interactive-2d/scenes/game.ts
@@ -9,20 +9,23 @@ export class Game extends Scene
 
     create ()
     {
-        this.cameras.main.setBackgroundColor(0x00ff00);
+        this.cameras.main.setBackgroundColor(0x000000);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+        // this.add.image(512, 384, 'background').setAlpha(0.5);
 
         this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
+            fontFamily: 'Arial Black',
+            fontSize: 38,
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
+        // this.input.once('pointerdown', () => {
 
-            this.scene.start('GameOver');
+        //     // this.scene.start('GameOver');
 
-        });
+        // });
     }
 }
