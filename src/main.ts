c57b60f7dd088632f17a0c4d852275b16b357978
beipym <payamb.wrk@gmail.com>
2025-05-08 17:20:42 +0330

    beautify

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import StartGame from './bg-interactive-2d/main.ts'
 
-document.addEventListener("DOMContentLoaded" , event=>{
+document.addEventListener("DOMContentLoaded" , event => {
   StartGame('app');
 })
 
